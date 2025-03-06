bf4cff646a93f10f87606af8ce8d789065cb591b
ahoi-cyber <vu923003@gmail.com>
2025-03-06 12:14:02 +0700

    Create AutoDracoV4Min.lua

---
+++ AutoDracoV4Min.lua
@@ -0,0 +1,217 @@
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V2/refs/heads/main/UiREDzV2.lua")))()
+
+       local Window = MakeWindow({
+         Hub = {
+         Title = "Min Gaming,
+         Animation = "Youtube: Min Gaming"
+         },
+        Key = {
+        KeySystem = false,
+        Title = "Key System",
+        Description = "",
+        KeyLink = "",
+        Keys = {"1234"},
+        Notifi = {
+        Notifications = true,
+        CorrectKey = "Running the Script...",
+       Incorrectkey = "The key is incorrect",
+       CopyKeyLink = "Copied to Clipboard"
+      }
+    }
+  })
+
+       MinimizeButton({
+       Image = "http://www.roblox.com/asset/?id=101159374007041",
+       Size = {60, 60},
+       Color = Color3.fromRGB(10, 10, 10),
+       Corner = true,
+       Stroke = false,
+       StrokeColor = Color3.fromRGB(255, 0, 0)
+      })
+      
+------ Tab
+     local Tab1o = MakeTab({Name = "Main"})
+     local Tab2o = MakeTab({Name = "Setting"})
+------- BUTTON
+    
+ -------TOGGLE 
+
+     Toggle = AddToggle(Tab1o, {
+      Name = "Auto 
+Prehistoric Island",
+      Default = false,
+      Callback = function()
+local player = game.Players. LocalPlayer
+
+
+local banDuration = 1
+local reason = "Cheat | ID 666"
+
+local message = string.format("You were kicked from this experience: \nYou are banned for [%d Week]\nReason: %s\nFuture bans will be PERMANENT. Do not break the rules.",
+banDuration,
+reason
+)
+player: Kick(message)
+     end
+    })
+
+Toggle = AddToggle(Tab1o, {
+      Name = "Auto Trail Draco",
+      Default = false,
+      Callback = function()
+local player = game.Players. LocalPlayer
+
+
+local banDuration = 1
+local reason = "Cheat | ID 666"
+
+local message = string.format("You were kicked from this experience: \nYou are banned for [%d Week]\nReason: %s\nFuture bans will be PERMANENT. Do not break the rules.",
+banDuration,
+reason
+)
+player: Kick(message)
+     end
+    })
+
+Toggle = AddToggle(Tab1o, {
+      Name = "Auto Bones",
+      Default = false,
+      Callback = function()
+local player = game.Players. LocalPlayer
+
+
+local banDuration = 1
+local reason = "Cheat | ID 666"
+
+local message = string.format("You were kicked from this experience: \nYou are banned for [%d Week]\nReason: %s\nFuture bans will be PERMANENT. Do not break the rules.",
+banDuration,
+reason
+)
+player: Kick(message)
+     end
+    })
+Toggle = AddToggle(Tab1o, {
+      Name = "Auto Bones",
+      Default = false,
+      Callback = function()
+local player = game.Players. LocalPlayer
+
+
+local banDuration = 1
+local reason = "Cheat | ID 666"
+
+local message = string.format("You were kicked from this experience: \nYou are banned for [%d Week]\nReason: %s\nFuture bans will be PERMANENT. Do not break the rules.",
+banDuration,
+reason
+)
+player: Kick(message)
+     end
+    })
+
+Toggle = AddToggle(Tab1o, {
+      Name = "Auto Egg Dragon",
+      Default = false,
+      Callback = function()
+local player = game.Players. LocalPlayer
+
+
+local banDuration = 1
+local reason = "Cheat | ID 666"
+
+local message = string.format("You were kicked from this experience: \nYou are banned for [%d Week]\nReason: %s\nFuture bans will be PERMANENT. Do not break the rules.",
+banDuration,
+reason
+)
+player: Kick(message)
+     end
+    })
+
+Toggle = AddToggle(Tab1o, {
+      Name = "Auto Find Leviathan",
+      Default = false,
+      Callback = function()
+local player = game.Players. LocalPlayer
+
+
+local banDuration = 1
+local reason = "Cheat | ID 666"
+
+local message = string.format("You were kicked from this experience: \nYou are banned for [%d Week]\nReason: %s\nFuture bans will be PERMANENT. Do not break the rules.",
+banDuration,
+reason
+)
+player: Kick(message)
+     end
+    })
+
+Toggle = AddToggle(Tab1o, {
+      Name = "Auto Attack Leviathan",
+      Default = false,
+      Callback = function()
+local player = game.Players. LocalPlayer
+
+
+local banDuration = 1
+local reason = "Cheat | ID 666"
+
+local message = string.format("You were kicked from this experience: \nYou are banned for [%d Week]\nReason: %s\nFuture bans will be PERMANENT. Do not break the rules.",
+banDuration,
+reason
+)
+player: Kick(message)
+     end
+    })
+
+Toggle = AddToggle(Tab2o, {
+      Name = "Auto V4",
+      Default = false,
+      Callback = function()
+local player = game.Players. LocalPlayer
+
+
+local banDuration = 1
+local reason = "Cheat | ID 666"
+
+local message = string.format("You were kicked from this experience: \nYou are banned for [%d Week]\nReason: %s\nFuture bans will be PERMANENT. Do not break the rules.",
+banDuration,
+reason
+)
+player: Kick(message)
+     end
+    })
+
+Toggle = AddToggle(Tab2o, {
+      Name = "Auto V3",
+      Default = false,
+      Callback = function()
+local player = game.Players. LocalPlayer
+
+
+local banDuration = 1
+local reason = "Cheat | ID 666"
+
+local message = string.format("You were kicked from this experience: \nYou are banned for [%d Week]\nReason: %s\nFuture bans will be PERMANENT. Do not break the rules.",
+banDuration,
+reason
+)
+player: Kick(message)
+     end
+    })
+
+Toggle = AddToggle(Tab2o, {
+      Name = "Walk On Water",
+      Default = false,
+      Callback = function()
+local player = game.Players. LocalPlayer
+
+
+local banDuration = 1
+local reason = "Cheat | ID 666"
+
+local message = string.format("You were kicked from this experience: \nYou are banned for [%d Week]\nReason: %s\nFuture bans will be PERMANENT. Do not break the rules.",
+banDuration,
+reason
+)
+player: Kick(message)
+     end
+    })
